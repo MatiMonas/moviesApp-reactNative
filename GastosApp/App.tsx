fdcd2801d83cfd711f758de4f5a496e7a8c0707c
Matías Monastirsky <81826117+MatiMonas@users.noreply.github.com>
2021-11-18 10:01:00 -0300

    native init

---
+++ GastosApp/App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, {FC} from 'react';
+import {Text, View} from 'react-native';
+
+const App: FC = () => {
+  console.log('hola');
+  return (
+    <View>
+      <Text>Hola</Text>
+    </View>
+  );
+};
+
+export default App;
--- MoviesApp/App.js
+++ MoviesApp/App.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './views/Home';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Home />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default App;
